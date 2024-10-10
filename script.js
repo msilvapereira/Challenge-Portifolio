939b399c608cfe229638ebf08afc7ae66c1eb1d7
Maria Eduarda <mariaeduarda.art18@gmail.com>
2024-10-10 15:20:47 -0300

    teste branch nova

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //A validação do formulário com javascript é considerado um desafio extra, não é preciso fazer esta validação para realizar sua entrega
+
+//git checkout "teste" para trocar a branch
